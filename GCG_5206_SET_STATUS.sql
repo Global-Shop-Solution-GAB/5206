0708291781b8b07e54d692cef61feaf1cc252b89
Service Web <sw@gssmail.com>
2024-03-13 13:45:40 -0500

    Commit changes in C:\Websites\Git\emarlow\5206\GCG_5206_SET_STATUS.sql

---
+++ GCG_5206_SET_STATUS.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5206_SET_STATUS
+ * ========================================== */
+CREATE TABLE "GCG_5206_SET_STATUS"(
+ "IDENTITY" IDENTITY DEFAULT '0',
+ "STATUS" CHAR(100) NOT NULL ,
+ UNIQUE ("IDENTITY"));
+
+
